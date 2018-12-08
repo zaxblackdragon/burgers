6789126d41893b0986c8036ff2e364aa646335e8
Zac Seewald <zaxblackdragon.com>
2018-12-08 12:40:31 -0600

    orm set up - needs functionality

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+const connection = require("./connection.js");
+
+const orm = {
+    /*
+    dont fully understand this and what happens here?
+    where the functions that work with the data from the database and updates to the browser live
+
+  --  INSRTRUCTIONS --
+In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+
+selectAll()
+insertOne()
+updateOne()
+*/
+}
+// Export the ORM object in module.exports.
+module.exports = orm;
