0bab9b79f9eb9308282ebf96c3bf2cbbd8166cad
Zac Seewald <zaxblackdragon.com>
2018-12-08 12:01:32 -0600

    updated schema, seed and recreated table

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,24 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
 
+-- In the db folder, create a file named schema.sql. Write SQL queries this file that do the following:
+
+DROP DATABASE IF EXISTS burgers_db;
+-- Create the burgers_db. DONE
+CREATE DATABASE burgers_db;
+-- Switch to or use the burgers_db.
+USE burgers_db;
+-- Create a burgers table with these fields:
+-- id: an auto incrementing int that serves as the primary key.
+-- burger_name: a string.
+-- devoured: a boolean.
 CREATE TABLE burgers (
     id INT(20) AUTO_INCREMENT PRIMARY KEY,
     burger_name VARCHAR(50) NOT NULL,
     devoure BOOLEAN
 );
+
+
+
+
+
+
 
--- db/seed.sql
+++ db/seed.sql
@@ -1,3 +1,5 @@
+-- Still in the db folder, create a seeds.sql file. In this file, write insert queries to populate the burgers table with at least three entries.
+USE burgers_db;
 INSERT INTO burgers (burger_name, devoure) VALUES ( "Cheese Burger", true);
 INSERT INTO burgers (burger_name, devoure) VALUES ( "Cherry Burger", false);
 INSERT INTO burgers (burger_name, devoure) VALUES ( "Harry Burger", false);
