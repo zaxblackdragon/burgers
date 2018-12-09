22885f1c880d6c4c5213b76343d54f39de8e202a
Zac Seewald <zaxblackdragon.com>
2018-12-09 15:29:59 -0600

    orm connected and working

--- config/orm.js
+++ config/orm.js
@@ -1,17 +1,38 @@
-const connection = require("./connection.js");
+var connection = require("./connection.js");
 
-const orm = {
+var showInConsole = function(results) {
+    console.log(results);
+};
+var orm = {
+  select: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
+   
+   
+//    select: function(whatToSelect, tableInput) {
+//     var queryString = "SELECT ?? FROM ??";
+//     connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
+//       if (err) throw err;
+//       console.log(result);
+//     });
     /*
-    dont fully understand this and what happens here?
-    where the functions that work with the data from the database and updates to the browser live
-
+    dont fully understand this and what happens here?   Translates from object orientation to table orientation
+    
   --  INSRTRUCTIONS --
 In the orm.js file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 
 selectAll()
 insertOne()
 updateOne()
+
 */
-}
+
 // Export the ORM object in module.exports.
 module.exports = orm;
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const express = require("express");
-const fs = require("fs");
-const app = express();
-const PORT = process.env.PORT || 3000;
+var express = require("express");
+var PORT = process.env.PORT || 3000;
+// const fs = require("fs");
+var app = express();
 
+var orm = require("./config/orm.js");
+var showInConsole = function(results) {
+  console.log(results);
+};
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes go here:
-
+orm.select("burgers", showInConsole);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
