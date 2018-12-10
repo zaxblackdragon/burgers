23507daa7e0e31b0ecf7694eda890177fa1e2954
Zac Seewald <zaxblackdragon.com>
2018-12-09 20:31:46 -0600

    localhost3000 workinggit add .

--- constrollers/burgerControl.js
+++ constrollers/burgerControl.js
@@ -11,29 +11,31 @@ router.get("/", function (req,res) {
         res.render("index", hbsObject);
     });
 });
-router.post("/api/burgers", function(req, res) {
-    burger.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    });
-  });
+
+module.exports = router;
+// router.post("/api/burgers", function(req, res) {
+//     burger.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
+//       // Send back the ID of the new quote
+//       res.json({ id: result.insertId });
+//     });
+//   });
   
-  router.put("/api/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
+//   router.put("/api/burgers/:id", function(req, res) {
+//     var condition = "id = " + req.params.id;
   
-    console.log("condition", condition);
+//     console.log("condition", condition);
   
-    burger.update(
-      {
-        sleepy: req.body.sleepy
-      },
-      condition,
-      function(result) {
-        if (result.changedRows === 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        }
-        res.status(200).end();
+//     burger.update(
+//       {
+//         sleepy: req.body.sleepy
+//       },
+//       condition,
+//       function(result) {
+//         if (result.changedRows === 0) {
+//           // If no rows were changed, then the ID must not exist, so 404
+//           return res.status(404).end();
+//         }
+//         res.status(200).end();
   
-      }
-    );
+//       }
+    // );
--- server.js
+++ server.js
@@ -3,20 +3,27 @@ const PORT = process.env.PORT || 3000;
 // const fs = require("fs");
 const app = express();
 
-const orm = require("./config/orm.js");
-const showInConsole = function(results) {
-  console.log(results);
-};
+//const orm = require("./config/orm.js");
+// const showInConsole = function(results) {
+//   console.log(results);
+// };
 
 // Middleware
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars")
 // Routes go here:
 
+const routes = require("./constrollers/burgerControl.js");
+
+app.use(routes);
 
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("YAY!!! App listening on PORT " + PORT);
   });
